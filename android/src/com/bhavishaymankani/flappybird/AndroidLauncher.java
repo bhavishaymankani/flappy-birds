a826a08672c95480aa3cb24275660de458b7e4d7
bhavishaymankani <bhavishay7@gmail.com>
2021-05-26 15:17:41 +0530

    commit

---
+++ android/src/com/bhavishaymankani/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.bhavishaymankani.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.bhavishaymankani.flappybird.FlappyBird;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FlappyBird(), config);
+	}
+}
